522e75932fdc4f245f3d82469cd2f1e5bd1e0ded
Allophycocyanin <amarcos777r@gmail.com>
2023-11-07 19:45:12 -0800

    real update

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -18,7 +18,7 @@ func _on_start_button_pressed():
 
 
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file('res://Scene/settings.tscn')
+	get_tree().change_scene_to_file('res://Scene/settings_menu.tscn')
 
 
 func _on_quit_button_pressed():
