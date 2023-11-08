dda19304d37be96dfb5c311b876bb448c866a05e
Allophycocyanin <amarcos777r@gmail.com>
2023-11-07 18:54:46 -0800

    Menu upgrade

--- main_menu.gd
+++ main_menu.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print('Main Menu Ready')
-	$Options/StartButton.grab_focus()
+	$StartButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,3 +19,7 @@ func _on_start_button_pressed():
 
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file('res://settings.tscn')
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
