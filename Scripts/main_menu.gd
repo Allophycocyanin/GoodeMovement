5f6d9e18befc878ed67cd093ee4f1aca8a0e0f11
Allophycocyanin <amarcos777r@gmail.com>
2023-11-09 16:07:27 -0800

    it works now

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -9,17 +9,12 @@ func _ready():
 	# Disable mouse input
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_start_button_pressed():
-	if Input.is_action_just_pressed('ui_accept'):
-		get_tree().change_scene_to_file('res://Scene/level_1.tscn')
-
-
+	get_tree().change_scene_to_file('res://Scene/level_1.tscn')
 
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file('res://Scene/settings_menu.tscn')
