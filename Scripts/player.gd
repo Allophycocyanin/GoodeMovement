a2e650fcc1b9b796da4e4439cdc47553d0ba672a
FrozenTrojan <114612365+ethanton0927@users.noreply.github.com>
2024-08-12 14:08:28 -0700

    Coyote Time

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
+@export var Coyote_Time: float = 0.3
 const JUMP_VELOCITY = -350.0
 const WALK = 150
 const RUN = 300
 const ACCELERATION = 10
 var	speed = 0
+var Jump_Available: bool = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,14 +16,20 @@ var sprinting = false
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
+		if Jump_Available:
+			get_tree().create_timer(Coyote_Time).timeout.connect(Coyote_Timeout)
+		
 		velocity.y += gravity * delta
+	else:
+		Jump_Available = true
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var sprint_pressed = Input.is_action_just_pressed('sprint')
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and Jump_Available:
+		Jump_Available = false
 		if sprinting:
 			velocity.y = JUMP_VELOCITY - ACCELERATION
 		else:
@@ -46,7 +54,9 @@ func _physics_process(delta):
 	
 	# player_animation()
 	move_and_slide()
-	#player_animation()
+
+func Coyote_Timeout():
+	Jump_Available = false
 	
 	# Handles Movement Animation
 func player_animation():
