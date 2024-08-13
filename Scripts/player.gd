5ca56126e368d02bdc0ac1c19c2f8ec3c11941bd
RealRevilo <102778852+RealRevilo@users.noreply.github.com>
2024-08-12 17:31:35 -0700

    Mousely
    Added Mousely

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,45 +1,30 @@
 extends CharacterBody2D
 
-#==================================================================================================
-# Variables
-#==================================================================================================
-@export var Coyote_Time: float = 0.05
 const JUMP_VELOCITY = -350.0
 const WALK = 150
-const RUN = 250
-const ACCELERATION = 7
+const RUN = 300
+const ACCELERATION = 10
 var	speed = 0
-var Jump_Available: bool = true
-var flip: bool = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var sprinting = false
 
 func _physics_process(delta):
 	#==============================================================================================
 	# Gravity Manager
 	#==============================================================================================
 	if not is_on_floor():
-		if Jump_Available:
-			get_tree().create_timer(Coyote_Time).timeout.connect(Coyote_Timeout)
-		
 		velocity.y += gravity * delta
-	else:
-		Jump_Available = true
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction: float = Input.get_action_strength("move_right") \
-					- Input.get_action_strength("move_left")
-	
-	# Checks if the sprint key is being pressed
-	var sprint_pressed = Input.is_action_pressed('sprint')
-	
-	#==============================================================================================
-	# Jump Manager
-	#==============================================================================================
-	if Input.is_action_just_pressed("jump") and Jump_Available:
-		Jump_Available = false
-		if sprint_pressed:
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var sprint_pressed = Input.is_action_just_pressed('sprint')
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		if sprinting:
 			velocity.y = JUMP_VELOCITY - ACCELERATION
 		else:
 			velocity.y = JUMP_VELOCITY
@@ -53,39 +38,22 @@ func _physics_process(delta):
 		if(speed > 0):
 			speed -= ACCELERATION
 	
-	
-	#==============================================================================================
-	# Horizontal Movement Manager
-	#==============================================================================================
-	if  sprint_pressed:
-		if(direction < 0.0 && speed > -RUN):
-			speed -= ACCELERATION * 2
-		if(direction > 0.0 && speed < RUN):
-			speed += ACCELERATION * 2
-	else:
-		if(direction < 0.0 && speed > -WALK):
-			speed -= ACCELERATION
-		if(direction > 0.0 && speed < WALK):
-			speed += ACCELERATION
-
-	# Sets the players X Velocity
-	velocity.x = speed
-	print("X: ", velocity.x, ", Speed: ", speed, ", Direction: ", direction)
+	player_animation()
 	move_and_slide()
-
-#==================================================================================================
-# Coyote Time
-#==================================================================================================
-func Coyote_Timeout():
-	Jump_Available = false
 	
 #==================================================================================================
 # Animation Handler
 #==================================================================================================
 func player_animation():
 	if velocity.x > 0:
-		$AnimatedSprite2D.play("walk_right")
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play('Mousely_walk_right')
 	elif velocity.x < 0:
-		$AnimatedSprite2D.play('walk_left')
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play('Mousely_walk_left')
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play('Mousely_fall')
+	elif velocity.y > 0:
+		$AnimatedSprite2D.play('Mousely_fall')
 	else:
-		$AnimatedSprite2D.play('fall')
+		$AnimatedSprite2D.play('Mousely_idle')
