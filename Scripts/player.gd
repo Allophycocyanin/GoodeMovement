ce88b807332184fca54b7e7cfd17105fb093d79f
RealRevilo <102778852+RealRevilo@users.noreply.github.com>
2024-08-12 17:31:35 -0700

    Mousely
    Added Mousely

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,34 +1,27 @@
 extends CharacterBody2D
 
-@export var Coyote_Time: float = 0.05
 const JUMP_VELOCITY = -350.0
 const WALK = 150
-const RUN = 250
-const ACCELERATION = 7
+const RUN = 300
+const ACCELERATION = 10
 var	speed = 0
-var Jump_Available: bool = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 var sprinting = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		if Jump_Available:
-			get_tree().create_timer(Coyote_Time).timeout.connect(Coyote_Timeout)
-		
 		velocity.y += gravity * delta
-	else:
-		Jump_Available = true
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var sprint_pressed = Input.is_action_just_pressed('sprint')
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and Jump_Available:
-		Jump_Available = false
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		if sprinting:
 			velocity.y = JUMP_VELOCITY - ACCELERATION
 		else:
@@ -51,17 +44,20 @@ func _physics_process(delta):
 			speed += ACCELERATION
 		velocity.x = direction *	speed
 	
-	# player_animation()
+	player_animation()
 	move_and_slide()
-
-func Coyote_Timeout():
-	Jump_Available = false
 	
 	# Handles Movement Animation
 func player_animation():
 	if velocity.x > 0:
-		$AnimatedSprite2D.play("walk_right")
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play('Mousely_walk_right')
 	elif velocity.x < 0:
-		$AnimatedSprite2D.play('walk_left')
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play('Mousely_walk_left')
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play('Mousely_fall')
+	elif velocity.y > 0:
+		$AnimatedSprite2D.play('Mousely_fall')
 	else:
-		$AnimatedSprite2D.play('fall')
+		$AnimatedSprite2D.play('Mousely_idle')
