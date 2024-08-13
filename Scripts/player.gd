3614830b1c351a93109ee4b76783cba13067ec40
RealRevilo <102778852+RealRevilo@users.noreply.github.com>
2024-08-12 18:19:09 -0700

    Update player.gd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,30 +1,45 @@
 extends CharacterBody2D
 
+#==================================================================================================
+# Variables
+#==================================================================================================
+@export var Coyote_Time: float = 0.05
 const JUMP_VELOCITY = -350.0
 const WALK = 150
-const RUN = 300
-const ACCELERATION = 10
+const RUN = 250
+const ACCELERATION = 7
 var	speed = 0
-
+var Jump_Available: bool = true
+var flip: bool = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var sprinting = false
 
 func _physics_process(delta):
 	#==============================================================================================
 	# Gravity Manager
 	#==============================================================================================
 	if not is_on_floor():
+		if Jump_Available:
+			get_tree().create_timer(Coyote_Time).timeout.connect(Coyote_Timeout)
+		
 		velocity.y += gravity * delta
+	else:
+		Jump_Available = true
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var sprint_pressed = Input.is_action_just_pressed('sprint')
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		if sprinting:
+	var direction: float = Input.get_action_strength("move_right") \
+					- Input.get_action_strength("move_left")
+	
+	# Checks if the sprint key is being pressed
+	var sprint_pressed = Input.is_action_pressed('sprint')
+	
+	#==============================================================================================
+	# Jump Manager
+	#==============================================================================================
+	if Input.is_action_just_pressed("jump") and Jump_Available:
+		Jump_Available = false
+		if sprint_pressed:
 			velocity.y = JUMP_VELOCITY - ACCELERATION
 		else:
 			velocity.y = JUMP_VELOCITY
@@ -32,14 +47,43 @@ func _physics_process(delta):
 	#==============================================================================================
 	# Idle Decceleration
 	#==============================================================================================
-	if (direction == 0.0 && speed != 0):
-		if(speed < 0):
-			speed += ACCELERATION
-		if(speed > 0):
-			speed -= ACCELERATION
+	if (direction == 0.0 && speed > 0):
+		speed -=ACCELERATION
 	
+	
+	#==============================================================================================
+	# Horizontal Movement Manager
+	#==============================================================================================
+	if((velocity.x > 0 && direction < 0.0) || (velocity.x < 0 && direction > 0.0)):
+		if(speed > 0 ):
+			speed -= ACCELERATION * 3
+			
+			
+	if sprint_pressed:
+		if	(speed <= RUN && (direction > 0.0 || direction < 0.0)):
+			speed += ACCELERATION*2
+	else:
+		# Walk speed
+		# Speed increases if moving in specified direction L|R
+		if	(speed <= WALK && (direction > 0.0 || direction < 0.0)):
+			speed += ACCELERATION
+	
+	#==============================================================================================
+	# Turning 
+	#==============================================================================================
+	
+
+	# Sets the players X Velocity
+	velocity.x = speed * direction
+	#print("X: ", velocity.x, ", Speed: ", speed, ", Direction: ", direction)
 	player_animation()
 	move_and_slide()
+
+#==================================================================================================
+# Coyote Time
+#==================================================================================================
+func Coyote_Timeout():
+	Jump_Available = false
 	
 #==================================================================================================
 # Animation Handler
